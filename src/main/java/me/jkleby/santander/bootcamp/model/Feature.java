cbf5ae6573f6f95202ea1097340acf20a1018685
Kleby <kleby10@gmail.com>
2023-10-11 15:09:00 -0300

    Adicionar model, repository e configurar o h2

---
+++ src/main/java/me/jkleby/santander/bootcamp/model/Feature.java
@@ -0,0 +1,10 @@
+package me.jkleby.santander.bootcamp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_features")
+public class Feature extends BaseItem {
+}
---
+++ src/main/java/me/jkleby/santander/bootcamp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package me.jkleby.santander.bootcamp.repository;
+
+import me.jkleby.santander.bootcamp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
